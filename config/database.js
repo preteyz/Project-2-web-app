0288922d303451d6bf4c54b6ad4ccc19100c7ee3
jihyekwak <jihyekwak53@gmail.com>
2022-01-15 01:09:51 -0800

    add boilerplate, connect to mongoDB

---
+++ config/database.js
@@ -0,0 +1,14 @@
+// require
+const mongoose = require("mongoose");
+// shortcut to mongoose.connection object, created by mongoose.connect
+const db = mongoose.connection;
+const dbUrl = process.env.DATABASE_URL;
+
+mongoose
+	.connect(dbUrl)
+	.then(() =>
+		console.log(
+			`MongoDB successfully connected at ${db.host}:${db.port}!`
+		)
+	)
+	.catch((err) => console.log(`MongoDB connection FAILED :( Error: ${err}`));
