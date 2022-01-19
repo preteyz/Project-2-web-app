07d5c3ace05ee54a1060d37e420eb1fed2fc435c
jihyekwak <jihyekwak53@gmail.com>
2022-01-19 00:56:32 -0800

    add crud for users

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
-    db.User.find(req.params.id)
+    db.User.findById(req.params.id)
         .populate("posts")
         .populate("comments")
         .exec((err, foundUser) => {
@@ -21,8 +21,73 @@ const show = (req, res) => {
         });
 };
 
+const newUser = (req, res) => {
+    res.render("users/new")
+} 
+
+const create = (req, res) => {
+    db.User.create(req.body, (err, createdUser) => {
+        if (err) return res.send(err)
+        return res.redirect("/users")
+    })
+}
+
+const edit = (req, res) => {
+    db.User.findById(req.params.id, (err, foundUser) => {
+        if (err) return res.send(err);
+        return res.render("users/edit", {
+            user : foundUser
+        })
+    }) 
+}
+
+const update = (req, res) => {
+    db.User.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: {
+                ...req.body,
+            }
+        },
+        { new : true },
+        (err, updatedUser) => {
+            if (err) return res.send(err);
+            return res.redirect(`/users/${updatedUser._id}`)
+        }
+    )
+}
+
+const destroy = (req, res) => {
+    db.User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
+        if (err) return res.send(err);
+        db.Post.deleteMany(
+            { user : deletedUser._id }, 
+            (err, deletedPosts) => {
+                console.log(deletedPosts);
+                db.Comment.deleteMany(
+                    { user : deletedPosts._id }, 
+                    (err, deletedComments) => {
+                        console.log(deletedComments);
+                        return res.redirect("/users");
+                    }
+                )
+            }
+        )
+    })
+}
+
+const isLoggedIn = (req, res) => {
+	if (req.isAuthenticated()) return next();
+	res.redirect("/auth/google");
+};
 
 module.exports = {
     index,
-    show
+    show,
+    newUser,
+    create,
+    edit,
+    update,
+    destroy,
+    isLoggedIn
 }
