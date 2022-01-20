cc626e1fda15e089159336c0500306edb086a97b
jihyekwak <jihyekwak53@gmail.com>
2022-01-20 14:22:12 -0800

    update posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -5,8 +5,8 @@ const ctrl = require("../controllers");
 router.get("/", ctrl.posts.index);
 router.get("/new", ctrl.posts.newPost);
 router.get("/:id", ctrl.posts.show);
+router.get("/:id/edit", ctrl.posts.edit);
 router.post("/", ctrl.posts.create);
 router.delete("/:id", ctrl.posts.destroy);
-
 
 module.exports = router;
