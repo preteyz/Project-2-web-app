91ee8dddbba23be39822c99162e8bac2093b041f
jihyekwak <jihyekwak53@gmail.com>
2022-01-21 17:51:24 -0800

    update routes and create uploaded file folder

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-
 const ctrl = require("../controllers");
+const upload = require("../middleware/upload")
 
 router.get("/", ctrl.posts.index);
 router.get("/new", ctrl.posts.newPost);
 router.get("/:id", ctrl.posts.show);
 router.get("/:id/edit", ctrl.posts.edit);
-router.post("/", ctrl.posts.create);
+router.post("/", upload.single('image'), ctrl.posts.create);
 router.put("/:id", ctrl.posts.update);
 router.delete("/:id", ctrl.posts.destroy);
 
